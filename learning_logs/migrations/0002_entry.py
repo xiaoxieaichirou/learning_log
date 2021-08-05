eb42802cab2fd8854cd85917062fd3bb10504f41
xiechunwei <13226512215@163.com>
2021-08-05 18:16:22 +0800

    修复entrys获取时间问题

--- learning_logs/migrations/0002_entry.py
+++ learning_logs/migrations/0002_entry.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
-                ('date_added', models.TextField()),
+                ('date_added', models.DateTimeField(auto_now_add=True)),
                 ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='learning_logs.topic')),
             ],
             options={
